60f4ebe1b7ac3388ee2701680670902a29a82d8e
Rajkishore Jena <rajkishorejena522@gmail.com>
2026-05-09 12:49:48 +0530

    Js Problem Best Time to Buy and Sell Stocks

--- Programing/Important-question.js
+++ Programing/Important-question.js
@@ -126,7 +126,6 @@ const twoSum = function (nums, target) {
 
 // console.log(twoSum([2, 7, 11, 15], 26));
 
-
 // Ques 5 - Best Time to Buy and Sell Stocks
 // You are given an array prices where prices[i] is the price of a given stock
 // on the ith day.
@@ -137,7 +136,41 @@ const twoSum = function (nums, target) {
 // Input: prices = [7, 1, 5, 3, 6, 4];  ----->>>>>  Output: 5;
 // Input: prices = [7, 6, 4, 3, 1];     ----->>>>>  Output: 0;
 
+// Brute Force Solution
+function maxProfit1(prices) {
+  let globalProfit = 0;
 
-function findMaxProfitOfStock(prices){
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const currentProfit = prices[j] - prices[i];
 
+      if (currentProfit > globalProfit) globalProfit = currentProfit;
+    }
+  }
+
+  return globalProfit;
 }
+
+// console.log(maxProfit1([7, 6, 4, 3, 1]));
+
+// Greedy Algorithm
+const maxProfit = function (prices) {
+  let minStockPurchasePrice = prices[0] || 0;
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minStockPurchasePrice) {
+      minStockPurchasePrice = prices[i];
+    }
+
+    profit = Math.max(profit, prices[i] - minStockPurchasePrice);
+  }
+
+  return profit;
+};
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
+
+// [7, 1, 5, 3, 6, 4]
+// min = 7 => 1
+// profit = 0 => 5-1 = 4 => 6-1 = 5
