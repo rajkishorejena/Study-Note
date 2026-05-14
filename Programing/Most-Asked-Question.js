22d4c47c3963a27c63d93e37862061f4c6b35dd4
Rajkishore Jena <rajkishorejena522@gmail.com>
2026-05-14 09:39:59 +0530

    Practiced 2 most asked question 14th May

---
+++ Programing/Most-Asked-Question.js
@@ -0,0 +1,83 @@
+//Q1: Flatten array
+// Using In-build method
+
+function flattenArrayInBuild(input) {
+  const result = input.flat(Infinity); // it will auto detect level and flat
+  // const result = input.flat(3); // how many deep level user want
+  return result;
+}
+
+// console.log(flattenArrayInBuild([1, 2, 3, [4, 5, [6]]]));
+// OutPut: [ 1, 2, 3, 4, 5, 6 ]
+// Time and space Complexity is 0(n) and 0(n)
+
+// Using Reduce
+function flattenArray(input) {
+  return input.reduce((acc, curr) => {
+    console.log(acc, curr);
+    if (Array.isArray(curr)) {
+      console.log("2", acc, curr);
+      return acc.concat(flattenArray(curr));
+    }
+    return acc.concat(curr);
+  }, []);
+}
+
+// console.log(flattenArray([1, 2, 3, [4, 5, [6]]]));
+//OutPut : [ 1, 2, 3, 4, 5, 6 ]
+// Time Complexity and space : O(n), O(n)
+
+// Without using In-Build Method
+function flattenArray(arr) {
+  let result = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+// const arr = [1, 2, [3, 4], [5, [6, 7]]];
+
+// console.log(flattenArray(arr));
+//Out Put: [1, 2, 3, 4, 5, 6, 7]
+// O(n)
+
+//Q2:  Find Second Largest Number in the Array
+
+//Using in-build method
+function findSecondLargestInBuild(input) {
+  const result = [...new Set(input)].sort((a, b) => b - a); // b-a : descending order // a-b : ascending order
+  return result[1];
+}
+
+// console.log(findSecondLargestInBuild([10, 5, 8, 20, 15]));
+//Output : 15
+// Time And Space Complexity  : O(n log n), O(n)
+
+// Without in-build method
+function findSecondLargest(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let num of arr) {
+    // console.log("l", largest, "s", secondLargest, "n", num);
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num !== largest) {
+      secondLargest = num;
+    }
+  }
+
+  return secondLargest;
+}
+
+console.log(findSecondLargest([10, 5, 8, 20, 15]));
+//Output : 15
+// Time And Space Complexity : O(n)
